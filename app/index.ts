740b82f243f072da6f2336d9d36077119829f06e
Johnny <araujjohnny@gmail.com>
2020-10-31 15:42:27 -0400

    Revert "flasy commit"
    This reverts commit 46baafdf60e03bbd1e1e01e6b0239fb0ded8eda0.

--- app/index.ts
+++ app/index.ts
@@ -1,25 +1,5 @@
 import pino from 'pino';
-import { createConnection } from 'typeorm';
-import { Player } from './entity/Player';
 
 const logger = pino();
 
-const startDb = async () => {
-  try {
-    const connection = await createConnection();
-    logger.info('connected');
-
-    const player = new Player();
-
-    player.name = 'Pedro Martinez';
-    player.jersey = 24;
-    player.isPlayerGood = true;
-
-    const player2 = new Player();
-    player2.name = 123;
-  } catch (err) {
-    logger.fatal('error connecting to db', err);
-  }
-};
-
-startDb();
+logger.info({ hello: 'world' });
